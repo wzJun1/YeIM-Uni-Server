6dbb5d28c92130b773a4c004e3047ae7ba77641c
wzJun <tzkj1688@foxmail.com>
2022-12-06 15:37:05 +0800

    update DataBase Import Bug

--- database.sql
+++ database.sql
@@ -7,8 +7,6 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
-CREATE DATABASE IF NOT EXISTS `yeim` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-USE `yeim`;
 
 CREATE TABLE `conversation` (
   `id` bigint(20) NOT NULL,
